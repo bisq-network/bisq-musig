070ea9c7ea21183933ee8bf091cadd0c65abc179
Steven Barclay <stejbac@gmail.com>
2025-04-25 23:58:51 +0100

    Use assert_cmd crate for CLI integration tests
    Add 'assert_cmd' and 'predicates' dev-dependencies to Cargo.toml for
    somewhat easier (and possibly more portable re. Windows line endings)
    integration testing with the 'musig-cli' binary client target. Refactor
    'tests/cli.rs' to use it in place of manual assertions for the exit code
    and stdout/stderr output of the client. Also add a 100ms timeout to all
    the CLI invocations, which permits the streaming 'notify-confidence'
    command (and associated 'RegisterConfidenceNtfn' RPC call) to be tested,
    albeit against an empty wallet (and thus a single "missing confidence"
    event is streamed before interrupt/timeout). Similarly, add a test for
    the 'list-unspent' command, currently returning an empty UTXO list.
    
    (Also run "cargo update" and bump the patch versions of a few of the
    project dependencies.)
    
    TODO: Use a mock 'WalletService' implementation (or real wallet service
     with some pre-persisted txs in its index) to test nonempty UTXO lists
     and slightly less trivial tx confidence streams.

--- rpc/tests/cli.rs
+++ rpc/tests/cli.rs
@@ -1,44 +1,60 @@
+use assert_cmd::Command;
+use assert_cmd::assert::Assert;
 use rpc::server::{WalletImpl, WalletServer};
 use rpc::wallet::WalletServiceImpl;
-use std::process::{Command, Output};
+use predicates::str;
 use std::sync::Arc;
+use std::time::Duration;
 use tokio::task::{self, JoinHandle};
 use tokio_util::task::AbortOnDropHandle;
 use tonic::transport::{self, Server};
 use tonic::transport::server::TcpIncoming;
 
-const EXPECTED_WALLET_BALANCE_RESPONSE: &[u8] = br#"{
+const CLI_TIMEOUT: Duration = Duration::from_millis(100);
+
+const EXPECTED_WALLET_BALANCE_RESPONSE: &str = r#"{
   "immature": 0,
   "trustedPending": 0,
   "untrustedPending": 0,
   "confirmed": 0
 }
 "#;
-const EXPECTED_NEW_ADDRESS_RESPONSE_1: &[u8] = br#"{
+const EXPECTED_NEW_ADDRESS_RESPONSE_1: &str = r#"{
   "address": "bcrt1pkar3gerekw8f9gef9vn9xz0qypytgacp9wa5saelpksdgct33qdqan7c89",
   "derivationPath": "m/86'/1'/0'/0/0"
 }
 "#;
-const EXPECTED_NEW_ADDRESS_RESPONSE_2: &[u8] = br#"{
+const EXPECTED_NEW_ADDRESS_RESPONSE_2: &str = r#"{
   "address": "bcrt1pv537m7m6w0gdrcdn3mqqdpgrk3j400yrdrjwf5c9whyl2f8f4p6q9dn3l9",
   "derivationPath": "m/86'/1'/0'/0/1"
+}
+"#;
+const EXPECTED_LIST_UNSPENT_RESPONSE: &str = r#"{
+  "utxos": []
+}
+"#;
+const EXPECTED_NOTIFY_CONFIDENCE_RESPONSE: &str = r#"{
+  "rawTx": null,
+  "confidenceType": "MISSING",
+  "numConfirmations": 0,
+  "confirmationBlockTime": null
 }
 "#;
 
 #[test]
 fn test_cli_usage() {
-    let output = exec_cli([]);
-    assert_eq!(output.status.code(), Some(2));
-    assert!(output.stdout.is_empty());
-    assert!(output.stderr.starts_with(b"Usage:"));
+    assert_cli([])
+        .code(2)
+        .stdout(str::is_empty())
+        .stderr(str::starts_with("Usage:"));
 }
 
 #[test]
 fn test_cli_no_connection() {
-    let output = exec_cli_with_port(50050, ["wallet-balance"]);
-    assert_eq!(output.status.code(), Some(1));
-    assert!(output.stdout.is_empty());
-    assert!(output.stderr.contains_subslice(b"ConnectError"));
+    assert_cli_with_port(50050, ["wallet-balance"])
+        .code(1)
+        .stdout(str::is_empty())
+        .stderr(str::contains("ConnectError"));
 }
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
@@ -46,11 +62,11 @@ async fn test_cli_wallet_balance() {
     let mut port = 50052;
     let _guard = AbortOnDropHandle::new(spawn_wallet_grpc_service(&mut port));
 
-    let output = task::spawn_blocking(move || exec_cli_with_port(port, ["wallet-balance"]))
-        .await.unwrap();
-    assert!(output.status.success());
-    assert_eq!(output.stdout, EXPECTED_WALLET_BALANCE_RESPONSE);
-    assert!(output.stderr.is_empty());
+    task::spawn_blocking(move || assert_cli_with_port(port, ["wallet-balance"]))
+        .await.unwrap()
+        .success()
+        .stdout(EXPECTED_WALLET_BALANCE_RESPONSE)
+        .stderr(str::is_empty());
 }
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
@@ -58,32 +74,59 @@ async fn test_cli_new_address() {
     let mut port = 50052;
     let _guard = AbortOnDropHandle::new(spawn_wallet_grpc_service(&mut port));
 
-    let output = task::spawn_blocking(move || exec_cli_with_port(port, ["new-address"]))
-        .await.unwrap();
-    assert!(output.status.success());
-    assert_eq!(output.stdout, EXPECTED_NEW_ADDRESS_RESPONSE_1);
-    assert!(output.stderr.is_empty());
+    task::spawn_blocking(move || assert_cli_with_port(port, ["new-address"]))
+        .await.unwrap()
+        .success()
+        .stdout(EXPECTED_NEW_ADDRESS_RESPONSE_1)
+        .stderr(str::is_empty());
 
-    let output = task::spawn_blocking(move || exec_cli_with_port(port, ["new-address"]))
-        .await.unwrap();
-    assert!(output.status.success());
-    assert_eq!(output.stdout, EXPECTED_NEW_ADDRESS_RESPONSE_2);
-    assert!(output.stderr.is_empty());
+    task::spawn_blocking(move || assert_cli_with_port(port, ["new-address"]))
+        .await.unwrap()
+        .success()
+        .stdout(EXPECTED_NEW_ADDRESS_RESPONSE_2)
+        .stderr(str::is_empty());
 }
 
-fn exec_cli<'a>(args: impl IntoIterator<Item=&'a str>) -> Output {
-    Command::new(env!("CARGO_BIN_EXE_musig-cli"))
+#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+async fn test_cli_list_unspent() {
+    let mut port = 50052;
+    let _guard = AbortOnDropHandle::new(spawn_wallet_grpc_service(&mut port));
+
+    task::spawn_blocking(move || assert_cli_with_port(port, ["list-unspent"]))
+        .await.unwrap()
+        .success()
+        .stdout(EXPECTED_LIST_UNSPENT_RESPONSE)
+        .stderr(str::is_empty());
+}
+
+//noinspection SpellCheckingInspection
+#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+async fn test_cli_notify_confidence() {
+    let mut port = 50052;
+    let _guard = AbortOnDropHandle::new(spawn_wallet_grpc_service(&mut port));
+
+    task::spawn_blocking(move || assert_cli_with_port(port, ["notify-confidence",
+        "37b560334094515cfdaa0146bfd4ce19e940064c505082031858b0aba3218990"]))
+        .await.unwrap()
+        .interrupted()
+        .stdout(EXPECTED_NOTIFY_CONFIDENCE_RESPONSE)
+        .stderr(str::is_empty());
+}
+
+fn assert_cli<'a>(args: impl IntoIterator<Item=&'a str>) -> Assert {
+    Command::cargo_bin("musig-cli").unwrap()
         .args(args)
-        .output().unwrap()
+        .timeout(CLI_TIMEOUT)
+        .assert()
 }
 
-fn exec_cli_with_port<'a>(port: u16, args: impl IntoIterator<Item=&'a str>) -> Output {
+fn assert_cli_with_port<'a>(port: u16, args: impl IntoIterator<Item=&'a str>) -> Assert {
     let port = port.to_string();
     #[expect(clippy::map_identity, reason = "change-of-lifetime false positive; see \
         https://github.com/rust-lang/rust-clippy/issues/9280")]
     let args = ["--port", &port].into_iter()
         .chain(args.into_iter().map(|s| s));
-    exec_cli(args)
+    assert_cli(args)
 }
 
 fn spawn_wallet_grpc_service(port: &mut u16) -> JoinHandle<Result<(), transport::Error>> {
@@ -101,14 +144,4 @@ fn spawn_wallet_grpc_service(port: &mut u16) -> JoinHandle<Result<(), transport:
             .serve_with_incoming(incoming)
             .await
     })
-}
-
-trait Searchable<T> {
-    fn contains_subslice(&self, subslice: &[T]) -> bool;
-}
-
-impl<T: PartialEq> Searchable<T> for [T] {
-    fn contains_subslice(&self, subslice: &[T]) -> bool {
-        self.windows(subslice.len()).any(|s| subslice.eq(s))
-    }
 }
