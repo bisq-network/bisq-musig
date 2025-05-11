d80ea3ba211e36ff9fce7007463d27f85a2373d2
Steven Barclay <stejbac@gmail.com>
2025-05-11 23:54:33 +0100

    Add '--port' command-line option to 'musigd'
    Make the bound port of the gRPC daemon configurable by using Clap to
    parse an optional "--port <PORT>" argument (with default 50051), just
    like the CLI client, instead of making it hardwired.
    
    This is mainly to facilitate local testing of the Bisq2 client against
    the Rust server, where there will typically be both an Alice and a Bob
    instance requiring their own respective servers, so that their trade
    states (and wallets) don't get mixed up when using the same trade ID.
    (The mock trade run by 'TradeProtocolClient' currently cheats by giving
    the buyer and seller separate trade IDs, so that their respective trade
    models can coexist inside a single 'musigd' instance.)

--- rpc/src/bin/musigd.rs
+++ rpc/src/bin/musigd.rs
@@ -1,11 +1,22 @@
-use rpc::wallet::WalletServiceImpl;
+use clap::Parser;
 use rpc::server::{MusigImpl, MusigServer, WalletImpl, WalletServer};
+use rpc::wallet::WalletServiceImpl;
 use std::sync::Arc;
 use tonic::transport::Server;
 
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+#[expect(clippy::doc_markdown, reason = "doc comments are used verbatim by Clap and not intended to be markdown")]
+struct Cli {
+    /// The port of the MuSig daemon
+    #[arg(short, long, default_value_t = 50051)]
+    port: u16,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:50051".parse()?;
+    let cli: Cli = Cli::parse();
+    let addr = format!("127.0.0.1:{}", cli.port).parse()?;
     let musig = MusigImpl::default();
     let wallet = WalletImpl { wallet_service: Arc::new(WalletServiceImpl::new()) };
     wallet.wallet_service.clone().spawn_connection();
