83868116c17db82623ad903d78f353b2495ef969
Steven Barclay <stejbac@gmail.com>
2025-08-31 22:18:24 +0800

    Add shuffling stability/portability regression tests
    Add unit tests to 'rpc::swap' for the 'Swap::shuffle' method, using the
    'ChaCha20Rng' PRNG, which should be stable and portable, to make sure
    that the same holds true for the shuffling itself. This is necessary to
    ensure that both peers will independently generate the same Deposit Tx,
    with deterministically shuffled inputs & outputs, from their exchanged
    buyer & seller PSBT halves.

--- rpc/src/swap.rs
+++ rpc/src/swap.rs
@@ -17,6 +17,9 @@ pub trait Swap: Sized {
                 if x / j_plus_1 < u32::MAX / j_plus_1 || u32::MAX % j_plus_1 == 0 {
                     break (x % j_plus_1) as usize;
                 }
+                // De-biasing branch. (This isn't actually all that useful for our purpose, because
+                // for any reasonable number of Deposit Tx inputs & outputs, one would likely need
+                // to carry out many billions of shuffles for any bias to start to become apparent.)
             };
             self = self.swap(i, j);
         }
@@ -61,5 +64,65 @@ impl Swap for &mut u32 {
             *self = i;
         }
         self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::SeedableRng as _;
+    use rand_chacha::ChaCha20Rng;
+
+    use super::*;
+
+    #[test]
+    fn test_unbiased_shuffle() {
+        // 8_997_264 is the smallest u32 causing the de-biasing branch of the shuffle algorithm to
+        // be hit, when using a seed of this form -- that is, the need to retry when taking r % LEN
+        // to rejection sample uniformly in the range 0..<LEN, because r was too close to u32::MAX.
+        let seed = [8_997_264_u32.to_le_bytes(); 8];
+        let mut rng = ChaCha20Rng::from_seed(seed.as_flattened().try_into().unwrap());
+
+        let mut xs = *b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        xs.shuffle(&mut rng);
+        assert_eq!(b"QFWHDYCTZVIUOBPESAMNRKJXLG", &xs);
+    }
+
+    #[test]
+    fn test_trivial_shuffles() {
+        let seed = [1u32.to_le_bytes(); 8];
+        let mut rng = ChaCha20Rng::from_seed(seed.as_flattened().try_into().unwrap());
+
+        let mut xs = *b"";
+        xs.shuffle(&mut rng);
+
+        let mut xs = *b"X";
+        xs.shuffle(&mut rng);
+        assert_eq!(b"X", &xs);
+    }
+
+    #[test]
+    fn test_shuffle_with_markers() {
+        let seed = [2u32.to_le_bytes(); 8];
+        let mut rng = ChaCha20Rng::from_seed(seed.as_flattened().try_into().unwrap());
+
+        let mut xs = *b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let [mut i_m, mut i_n] = [12, 13u32];
+        (xs.as_mut_slice(), (&mut i_m, &mut i_n)).shuffle(&mut rng);
+
+        assert_eq!(b"XIVHYQLEPRBGSZKTWNMOFADCJU", &xs);
+        assert_eq!(b"MN", &[xs[i_m as usize], xs[i_n as usize]]);
+    }
+
+    #[test]
+    fn test_parallel_shuffle() {
+        let seed = [3u32.to_le_bytes(); 8];
+        let mut rng = ChaCha20Rng::from_seed(seed.as_flattened().try_into().unwrap());
+
+        let mut xs = *b"ABCDEFGHI";
+        let mut ys = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        (xs.as_mut_slice(), ys.as_mut_slice()).shuffle(&mut rng);
+
+        assert_eq!(b"BIEAHGCFD", &xs);
+        assert_eq!([2, 9, 5, 1, 8, 7, 3, 6, 4], ys);
     }
 }
