248eb4b7291630868bbde2b31760da1ab192849b
Chris Sonek <csonek@proton.me>
2025-10-01 19:09:06 +0200

    changed formatting of comment to ///

--- wallet/src/lib.rs
+++ wallet/src/lib.rs
@@ -388,22 +388,20 @@ impl DerefMut for BMPWallet<Connection> {
         &mut self.wallet
     }
 }
-/*
-The Protocol Wallet API is used by the protocol to create and sign transactions.
-It's the part of functionality being exposed only to the protocol.
-The protocol will see `ProtocolWalletApi` and the GUI will see `WalletApi`, both are implemented in the BMPWallet.
- */
+
+/// The Protocol Wallet API is used by the protocol to create and sign transactions.
+/// It's the part of functionality being exposed only to the protocol.
+/// The protocol will see `ProtocolWalletApi` and the GUI will see `WalletApi`, both are implemented in the BMPWallet.
 pub trait ProtocolWalletApi {
     fn network(&self) -> Network;
 
     fn new_address(&mut self) -> anyhow::Result<Address>;
 
-    /* this creates a PSBT for use with the depositTx (but not limited to). You specify the receipients, consisting of the
-    deposit- (and trade-)amount and spk, and the trade_fee_outputs.
-    This method returns a PSBT with added inputs sufficient to pay the outputs and an optional change output.
-    NOTE: There might be no change output, if not needed.
-    The method guarantees that it won't reorder the outputs.
-     */
+    ///  this creates a PSBT for use with the depositTx (but not limited to). You specify the receipients, consisting of the
+    /// deposit- (and trade-)amount and spk, and the trade_fee_outputs.
+    /// This method returns a PSBT with added inputs sufficient to pay the outputs and an optional change output.
+    /// NOTE: There might be no change output, if not needed.
+    /// The method guarantees that it won't reorder the outputs.
     fn create_psbt(
         &mut self,
         recipients: Vec<(ScriptBuf, Amount)>,
